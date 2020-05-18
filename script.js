3b702b1998c8dc2c73a45a8b27e351a8624b89e7
ama1ia <45046116+ama1ia@users.noreply.github.com>
2020-05-17 17:35:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,63 @@
 
 //Popup boxes appear and disappear
-function firstImage() {
   var popup1 = document.getElementById("ET");
+  var popup2 = document.getElementById("LToP");
+  var popup3 = document.getElementById("SoL");
+  var popup4 = document.getElementById("StMB");
+  var popup5 = document.getElementById("CtR");
+  var popup6 = document.getElementById("AdT");
+  var popup7 = document.getElementById("BhP");
+  var popup8 = document.getElementById("C");
+  var popup9 = document.getElementById("GGB");
+  var popup10 = document.getElementById("GWoC");
+  var popup11 = document.getElementById("MP");
+  var popup12 = document.getElementById("TMh");
+
+function firstImage() {
   popup1.classList.toggle("show");
 }
 
 function secondImage() {
-  var popup2 = document.getElementById("LToP");
   popup2.classList.toggle("show");
 }
 
 function thirdImage() {
-  var popup3 = document.getElementById("SoL");
   popup3.classList.toggle("show");
 }
 
 function fourthImage() {
-  var popup4 = document.getElementById("StMB");
   popup4.classList.toggle("show");
 }
 
 function fifthImage() {
-  var popup5 = document.getElementById("CtR");
   popup5.classList.toggle("show");
 }
 
 function sixthImage() {
-  var popup6 = document.getElementById("AdT");
   popup6.classList.toggle("show");
 }
 
 function seventhImage() {
-  var popup7 = document.getElementById("BhP");
   popup7.classList.toggle("show");
 }
 
 function eighthImage() {
-  var popup8 = document.getElementById("C");
   popup8.classList.toggle("show");
 }
 
 function ninthImage() {
-  var popup9 = document.getElementById("GGB");
   popup9.classList.toggle("show");
 }
 
 function tenthImage() {
-  var popup10 = document.getElementById("GWoC");
   popup10.classList.toggle("show");
 }
 
 function eleventhImage() {
-  var popup11 = document.getElementById("MP");
   popup11.classList.toggle("show");
 }
 
 function twelfthImage() {
-  var popup12 = document.getElementById("TMh");
   popup12.classList.toggle("show");
 }
 
